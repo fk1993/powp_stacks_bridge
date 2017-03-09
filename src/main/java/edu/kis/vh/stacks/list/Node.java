6e8abdbe73dd32a44fdd35cdbd4a68bf1ed20f03
Filip Kaszuba <fkaszuba93@gmail.com>
2017-03-09 18:14:51 +0100

    3.1.8.

--- src/main/java/edu/kis/vh/stacks/list/Node.java
+++ src/main/java/edu/kis/vh/stacks/list/Node.java
@@ -2,11 +2,37 @@ package edu.kis.vh.stacks.list;
 
 public class Node {
 
-	int value;
-	Node prev, next;
+	private int value;
+	private Node prev;
+	private Node next;
+	//3.1.8. bezpośrednie odwołania do atrybutów zostały zastąpione getterami i setterami
 
 	public Node(int i) {
 		value = i;
+	}
+
+	int getValue() {
+		return value;
+	}
+
+	void setValue(int value) {
+		this.value = value;
+	}
+
+	Node getPrev() {
+		return prev;
+	}
+
+	void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	Node getNext() {
+		return next;
+	}
+
+	void setNext(Node next) {
+		this.next = next;
 	}
 
 }
