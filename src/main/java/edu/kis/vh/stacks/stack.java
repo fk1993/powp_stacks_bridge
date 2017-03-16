d94dfe61b80e2cf920ea5841100fedade6bf0f83
Filip Kaszuba <fkaszuba93@gmail.com>
2017-03-16 12:55:22 +0100

    3.3.2.

--- src/main/java/edu/kis/vh/stacks/stack.java
+++ src/main/java/edu/kis/vh/stacks/stack.java
@@ -1,5 +1,10 @@
 package edu.kis.vh.stacks;
 
+/**
+ * Standardowa implementacja stosu.
+ *
+ */
+
 public class Stack {
 
 	private static final int EMPTY = -1;
@@ -10,29 +15,47 @@ public class Stack {
 
 	private int total = EMPTY;
 	
+	/**
+	 * @return liczba elementów na stosie
+	 */
 	public int getTotal() {
 		return total;
 	}
 
+	/**
+	 * @param i - element dodawany do stosu
+	 */
 	public void push(int i) {
 		if (!isFull())
 			ITEMS[++total] = i;
 	}
 	
+	/**
+	 * @return true, jeśli stos jest pusty
+	 */
 	public boolean isEmpty() {
 		return total == EMPTY;
 	}
 	
+	/**
+	 * @return true, jeśli stos jest pełny
+	 */
 	public boolean isFull() {
 		return total == SIZE - 1;
 	}
 	
+	/**
+	 * @return element znajdujący się na szczycie stosu
+	 */
 	public int top() {
 		if (isEmpty())
 			return EMPTY;
 		return ITEMS[total];
 	}
 	
+	/**
+	 * @return element usunięty ze stosu
+	 */
 	public int pop() {
 		if (isEmpty())
 			return EMPTY;
