3e0e626592019c42323aaec2b71d5151cd5a6771
fk1993 <fkaszuba93@gmail.com>
2017-03-23 14:39:27 +0100

    lab2 3.1.1.

---
+++ src/main/java/StackArray.java
@@ -0,0 +1,58 @@
+
+public class StackArray {
+
+	private static final int EMPTY = -1;
+
+	private static final int SIZE = 12;
+
+	private final int[] ITEMS = new int[SIZE];	
+
+	private int total = EMPTY;
+	
+	/**
+	 * @return liczba elementów na stosie
+	 */
+	public int getTotal() {
+		return total;
+	}
+
+	/**
+	 * @param i - element dodawany do stosu
+	 */
+	public void push(int i) {
+		if (!isFull())
+			ITEMS[++total] = i;
+	}
+	
+	/**
+	 * @return true, jeśli stos jest pusty
+	 */
+	public boolean isEmpty() {
+		return total == EMPTY;
+	}
+	
+	/**
+	 * @return true, jeśli stos jest pełny
+	 */
+	public boolean isFull() {
+		return total == SIZE - 1;
+	}
+	
+	/**
+	 * @return element znajdujący się na szczycie stosu
+	 */
+	public int top() {
+		if (isEmpty())
+			return EMPTY;
+		return ITEMS[total];
+	}
+	
+	/**
+	 * @return element usunięty ze stosu
+	 */
+	public int pop() {
+		if (isEmpty())
+			return EMPTY;
+		return ITEMS[total--];
+	}
+}
