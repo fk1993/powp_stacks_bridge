b95741cb3fd88b6346a735e0d82cff4ceee15f03
Filip Kaszuba <fkaszuba93@gmail.com>
2017-03-30 12:34:02 +0200

    lab2 3.1.12.

--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -1,6 +1,18 @@
 package edu.kis.vh.stacks;
 
+import edu.kis.vh.stacks.implementations.StackArray;
+
 public class StackFIFO extends Stack {
+
+	public StackFIFO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StackFIFO(StackArray array) {
+		super(array);
+		// TODO Auto-generated constructor stub
+	}
 
 	private final Stack temp = new Stack();
 	
--- src/main/java/edu/kis/vh/stacks/StackHanoi.java
+++ src/main/java/edu/kis/vh/stacks/StackHanoi.java
@@ -1,6 +1,18 @@
 package edu.kis.vh.stacks;
 
+import edu.kis.vh.stacks.implementations.StackArray;
+
 public class StackHanoi extends Stack {
+
+	public StackHanoi() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StackHanoi(StackArray array) {
+		super(array);
+		// TODO Auto-generated constructor stub
+	}
 
 	private int totalRejected = 0;
 
